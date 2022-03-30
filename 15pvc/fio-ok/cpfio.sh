127efda5db9efdb2d34c35ca09b7ff81d42a067d
yanmingx-jiang <yanmingx.jiang@intel.com>
2022-03-30 16:28:40 +0800

    add fio

---
+++ 15pvc/fio-ok/cpfio.sh
@@ -0,0 +1,14 @@
+kubectl cp rand-read.fio fio-1:/root/rand-read.fio 
+kubectl cp rand-write.fio fio-1:/root/rand-write.fio 
+kubectl cp seq-read.fio fio-1:/root/seq-read.fio 
+kubectl cp seq-write.fio fio-1:/root/seq-write.fio 
+
+kubectl cp rand-read.fio fio-2:/root/rand-read.fio 
+kubectl cp rand-write.fio fio-2:/root/rand-write.fio 
+kubectl cp seq-read.fio fio-2:/root/seq-read.fio 
+kubectl cp seq-write.fio fio-2:/root/seq-write.fio 
+
+kubectl cp rand-read.fio fio-3:/root/rand-read.fio 
+kubectl cp rand-write.fio fio-3:/root/rand-write.fio 
+kubectl cp seq-read.fio fio-3:/root/seq-read.fio 
+kubectl cp seq-write.fio fio-3:/root/seq-write.fio 
